873f0b19577694569d29b57428859d6513fae917
Jonathan Pepler <jonpepler@Jons-MBP.lan>
2017-08-27 23:42:54 +0100

    Require key and generate key on map creation

--- app/models/map.rb
+++ app/models/map.rb
@@ -1,3 +1,16 @@
 class Map < ApplicationRecord
   validates :name, :presence => true
+  validates :key, :presence => true
+
+  before_create :generate_key
+
+  def generate_key
+    key_check = true
+    key = ""
+    while key_check
+      key = SecureRandom.urlsafe_base64 25
+      key_check = false unless Map.find_by_key key
+    end
+    self.key = key
+  end
 end
