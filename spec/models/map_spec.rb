9d7846f10641cb879c292f21eca4f8431c9f4799
Jonathan Pepler <jonpepler@Jons-MBP.lan>
2017-08-27 10:54:12 +0100

    Add key requirement to Map spec

--- spec/models/map_spec.rb
+++ spec/models/map_spec.rb
@@ -7,3 +7,11 @@ RSpec.describe Map, :type => :model do
     expect(map.name).to eq("Sam Sheehy")
   end
 end
+
+RSpec.describe Map, :type => :model do
+  it "creates a unique key on creation" do
+    map = Map.create!(name: "Sam Sheehy")
+
+    expect(map.key).to exist
+  end
+end
